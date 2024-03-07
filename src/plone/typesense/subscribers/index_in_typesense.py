06e78e430bfb33e57b67488a231a5949be133909
MrTango <md@derico.de>
2024-03-07 17:25:40 +0100

    TS subscriber

--- src/plone/typesense/subscribers/index_in_typesense.py
+++ src/plone/typesense/subscribers/index_in_typesense.py
@@ -4,3 +4,7 @@
 def handler(obj, event):
     """Event handler"""
     print("{0} on object {1}".format(event.__class__, obj.absolute_url()))
+    # users_can_view: []
+    # all fields without fieldlevel permission
+    # all fields with fieldlevel permission
+
